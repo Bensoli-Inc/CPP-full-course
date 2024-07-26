432e26d0bd3fc830bbba15184a5ded9401150c62
Bensoli <benswambua96@gmail.com>
2024-07-26 15:56:12 +0300

    iostream and std cout & endl

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Bensoli " << std::endl;
+    std::cout << "Hello Bensoli " << std::endl;
+    std::cout << "Hello Bensoli " << std::endl;
+    std::cout << "Hello Bensoli " << std::endl;
+    std::cout << "Hello Bensoli " << std::endl;
+    std::cout << "Hello Bensoli " << std::endl;
+    std::cout << "Hello Bensoli " << std::endl;
+    std::cout << "Hello Bensoli " << std::endl;
+    std::cout << "Hello Bensoli " << std::endl;
+
+    return 0;
+}
